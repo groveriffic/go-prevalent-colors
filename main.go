2a6b7a9cc61c97778760a55fcef174bd7fdcdb72
Sam Ehlers <groveriffic@gmail.com>
2019-01-04 14:30:26 -0600

    Added RGB Color struct

--- main.go
+++ main.go
@@ -45,3 +45,33 @@ func eachPixel(img image.Image, f func(int, int, color.Color)) {
 		}
 	}
 }
+
+// RGB is a subset of image/color.RGBA
+// where we ignore the alpha channel
+type RGB struct {
+	R uint8
+	G uint8
+	B uint8
+}
+
+// NewRGB converts any color.Color to RGB.  Conversion may be lossy.
+func NewRGB(c color.Color) RGB {
+	c = color.RGBAModel.Convert(c)
+	rgba, ok := c.(color.RGBA)
+	if !ok {
+		// This type assertion should always succeed
+		// based on https://godoc.org/image/color#Model
+		// If we see this panic triggered in practice we might need to revisit.
+		panic("color conversion failure")
+	}
+	return RGB{
+		R: rgba.R,
+		G: rgba.G,
+		B: rgba.B,
+	}
+}
+
+// String formats color as six digit hexidecimal
+func (c RGB) String() string {
+	return fmt.Sprintf("#%02X%02X%02X", c.R, c.G, c.B)
+}
--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"image"
 	"image/color"
 	_ "image/jpeg"
@@ -34,4 +35,39 @@ func TestEachPixel(t *testing.T) {
 	if i != 100 {
 		t.Error("expected function to be called exactly 100 times")
 	}
+}
+
+func ExampleRGB_String() {
+	c := RGB{R: 0, G: 0, B: 0}
+	fmt.Println(c.String())
+
+	c = RGB{R: 0, G: 255, B: 0}
+	fmt.Println(c.String())
+
+	c = RGB{R: 255, G: 255, B: 255}
+	fmt.Println(c.String())
+
+	// Output:
+	// #000000
+	// #00FF00
+	// #FFFFFF
+}
+
+var _ fmt.Stringer = RGB{} // Interface check
+
+func ExampleNewRGB() {
+	rgb := NewRGB(color.Gray{Y: 0})
+	fmt.Println(rgb.String())
+
+	rgb = NewRGB(color.Gray{Y: 255})
+	fmt.Println(rgb.String())
+
+	// YCbCr is color model used by JPEG
+	rgb = NewRGB(color.YCbCr{Y: 255, Cb: 255, Cr: 255})
+	fmt.Println(rgb.String())
+
+	// Output:
+	// #000000
+	// #FFFFFF
+	// #FF79FF
 }
